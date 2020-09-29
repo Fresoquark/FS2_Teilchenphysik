abf292b1e696b01a86332bcdf880e39643843193
Nils Breer <nilsbreer@icloud.com>
2020-09-29 13:48:16 +0200

    classifier RandomForest ist drin, shuffle macht weirdes zeug, pymrmr ist einfach kacke

--- IceCube/code/data_cleaning.py
+++ IceCube/code/data_cleaning.py
@@ -1,20 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
+import pymrmr
 
 # daten einlesen als csv
-sig = pd.read_csv('.csv', delimiter=',')
-bkg = pd.read_csv('.csv', delimiter=',')
-
-# daten einlesen als hdf5,
-sig = pd.read_hdf('.h5', key='')
-sig = pd.read_hdf('.h5', key='')
-
-# features droppen die true daten erhalten
-sig.drop([''], axis=1, inplace=True)
-bkg.drop([''], axis=1, inplace=True)
-
-remove_NaN = np.nan_to_num(array_welches_nan_enthaellt)
-for i in range(len(array_with_inf)):
-    if np.isinf(array_with_inf[i]):
-        array_with_inf[i] = 0
+# sig = pd.read_csv('.csv', delimiter=',')
+# bkg = pd.read_csv('.csv', delimiter=',')
+#
+# # daten einlesen als hdf5,
+# sig = pd.read_hdf('.h5', key='')
+# sig = pd.read_hdf('.h5', key='')
+#
+# # features droppen die true daten erhalten
+# sig.drop([''], axis=1, inplace=True)
+# bkg.drop([''], axis=1, inplace=True)
+#
+# remove_NaN = np.nan_to_num(array_welches_nan_enthaellt)
+# for i in range(len(array_with_inf)):
+#     if np.isinf(array_with_inf[i]):
+#         array_with_inf[i] = 0
+#
+#
+# signal = signal.columns.tolist()
+# bkg = bkg.columns.tolist()
+#
+# for i in signal:
+#     if i not in bkg:
+#         signal.drop(i, axis = 1)
