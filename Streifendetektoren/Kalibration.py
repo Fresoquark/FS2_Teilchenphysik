2a7c4ba432cfa1394a6d3ed4bf1001662473d35c
Nicole Schulte <nicole.schulte@tu-dortmund.de>
2020-10-15 16:50:26 +0200

    Kalibrationsaufgabe vollständig

--- Streifendetektoren/Kalibration.py
+++ Streifendetektoren/Kalibration.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import constants
+from scipy import optimize
 from scipy.constants import elementary_charge
 
 zeit, counts = np.genfromtxt("Data/average_signal.txt",skip_header=2, unpack = True)
@@ -74,3 +75,52 @@ plt.xlabel("Energie in MeV")
 plt.ylabel("ADC")
 plt.savefig("plots/calib_channel40_0V.pdf")
 plt.clf()
+
+plt.plot(energie4_0V, ADC4_0V, marker = '.',  color = 'magenta', linestyle = 'None', label="Kanal 40 für 0V")
+plt.plot(energie4, ADC4, marker = '.',  color = 'dodgerblue', linestyle = 'None', label="Kanal 40 $U > U_{dep}$")
+plt.legend()
+plt.grid()
+plt.xlabel("Energie in MeV")
+plt.ylabel("ADC")
+plt.savefig("plots/calib_channel40_comparison.pdf")
+plt.clf()
+
+mean_ADC = []
+mean_pulses = []
+
+for i in range(len(ADC1)):
+    ADC_summe = ADC1[i] + ADC2[i] + ADC3[i] +ADC4[i]
+    mean_A = ADC_summe / 4 
+    mean_ADC.append(mean_A)
+
+for i in range(len(energie1)):
+    e_summe = energie1[i] + energie2[i] + energie3[i] +energie4[i]
+    mean_e = e_summe / 4 
+    mean_pulses.append(mean_e)
+
+mean_fit_ADC = mean_ADC[:140]
+mean_fit_pulses = mean_pulses[:140]
+
+
+def poly4(mean_fit_ADC, a , b, c, d, e):
+    return a + (np.array(mean_fit_ADC) * b) + ((np.array(mean_fit_ADC)**2) * c) + ((np.array(mean_fit_ADC)**3) * d) + ((np.array(mean_fit_ADC)**4) * e)
+
+params, params_covariance = optimize.curve_fit(poly4, mean_fit_ADC, mean_fit_pulses)
+
+print("#####################################################################################")
+print("Die Params haben die Dimension MeV")
+print("Das Polynom der Form: a + bx + cx**2 + dx**3 + ex**4 besitzt die folgenden Parameter:")
+print("Parameter a: ", params[0])
+print("Parameter b: ", params[1])
+print("Parameter c: ", params[2])
+print("Parameter d: ", params[3])
+print("Parameter e: ", params[4])
+print("#####################################################################################")
+
+plt.plot(mean_ADC, mean_pulses , marker = '.',  color = 'magenta', linestyle = 'None', label="Mittelwert der Elektronenpulse", alpha = 0.4)
+plt.plot(mean_fit_ADC, poly4(mean_fit_ADC, params[0], params[1], params[2], params[3], params[4]), label='Polynom 4. Grades', color = "dodgerblue", linewidth = 2.5)
+plt.legend()
+plt.grid()
+plt.ylabel("Energie in MeV")
+plt.xlabel("ADC")
+plt.savefig("plots/calib_mean.pdf")
