1646ee4fa8ecec08bb727cc6c8ec51554f3ae90d
Nils Breer <nilsbreer@icloud.com>
2020-10-21 14:10:15 +0200

    si detector weiter

--- Streifendetektoren/CCEL.py
+++ Streifendetektoren/CCEL.py
@@ -2,12 +2,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numpy.random import normal
 import os
+from scipy import constants
+from scipy import optimize
+from scipy.constants import elementary_charge
 
 Spannung_liste = [0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200]
 mean = []
 
 for i in Spannung_liste:
-    signal = np.genfromtxt("{}CCEL.txt".format(i), unpack = True)
+    signal = np.genfromtxt("Data/CCEL/{}CCEL.txt".format(i), unpack = True)
     mean.append(np.mean(signal))
 
 a = 0.00864769757886111
@@ -16,10 +19,38 @@ c = 2.0147268211332093e-05
 d = -1.4001913507631864e-07
 e = 3.474112787015162e-10
 
-umrechnung = []
+dicke = 300*10**(-6)# 300 meter
+depletionsSpannung = 70 # volt
 
-for i in means: 
-    calc = a + b* i + c* i**2 + d* i**3 + e* i**4
-    umrechnung.append(calc)
-umrechnung = np.sort(umrechnung)
+norm = mean / max(mean)
 
+def U_dep(D, u_dep, x):
+    if x < u_dep:
+        return D * np.sqrt(x / u_dep)
+    else:
+        return D
+
+spann = []
+for u in Spannung_liste:
+    spann.append(U_dep(dicke, depletionsSpannung, u))
+
+
+def fit_func(spann, a):
+    return (1 - np.exp(-np.array(spann) / a)) / (1 - np.exp(-300*10**(-6) / a))
+
+params, params_covariance = optimize.curve_fit(fit_func, spann, norm)
+
+print("#####################################################################################")
+print("Parameter a: ", params[0]*10**6, ' micrometer')
+# print("Parameter a: ", params[1], 'micrometer')
+print("#####################################################################################")
+
+plt.plot(Spannung_liste, norm, 'r.', label='Mittelwerte laser')
+plt.plot(Spannung_liste, fit_func(spann, *params), label='CCE', color = "dodgerblue", linewidth = 0.5)
+plt.legend()
+plt.xlabel('Spannung')
+plt.ylabel('CCEL')
+plt.savefig('plots/CCEL.pdf')
+'''
+Parameter a:  194.53707349139205  micrometer
+'''
--- Streifendetektoren/Quellscan.py
+++ Streifendetektoren/Quellscan.py
@@ -1,12 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import os
 
 clustersize = np.genfromtxt('Data/cluster_size.txt', unpack=True)
 
 Kanalnummer = np.arange(0,128,1)
 
-plt.plot(Kanalnummer[:20], clustersize[:20], marker = '.',  color = 'magenta', linestyle = 'None', label="Clusterhäufigkeit pro Event")
+plt.plot(Kanalnummer[:6], clustersize[:6], marker = '.',  color = 'magenta', linestyle = 'None', label="Clusterhäufigkeit pro Event")
 plt.legend()
 plt.grid()
 plt.xlabel("Kanal")
@@ -14,7 +14,7 @@ plt.ylabel("Häufigkeit")
 plt.savefig("plots/Clustersize_continuous.pdf")
 plt.clf()
 
-plt.bar(Kanalnummer[:20], clustersize[:20], label="Clustersize pro Event", color = 'magenta', alpha=0.6)
+plt.bar(Kanalnummer[:6], clustersize[:6], label="Kanäle pro Cluster", color = 'magenta', alpha=0.6)
 plt.legend()
 plt.grid()
 plt.xlabel("Kanal")
@@ -27,12 +27,41 @@ file = open("Data/Cluster_adc_entries.txt")
 
 number_of_entries = []
 
-for line in file: 
+for line in file:
     entries = line.split()
     number_of_entries.append(len(entries))
 
-plt.hist(number_of_entries, bins=17, range=[0, 16], label="Clustersize aller Kanäle", color = 'magenta', alpha=0.6)
-plt.bar(Kanalnummer[:20], clustersize[:20], label="Clustersize pro Event", color = 'magenta', alpha=0.6)
+# N0 = 0
+# N1 = 0
+# N2 = 0
+# N3 = 0
+# N4 = 0
+# N5 = 0
+# N6 = 0
+# N7 = 0
+# for i in number_of_entries:
+#     if i == 0:
+#         N0 += 1
+#     elif i == 1:
+#         N1 += 1
+#     elif i == 2:
+#         N2 += 1
+#     elif i == 3:
+#         N3 += 1
+#     elif i == 4:
+#         N4 += 1
+#     elif i == 5:
+#         N5 += 1
+#     elif i == 6:
+#         N6 += 1
+#     elif i == 7:
+#         N7 += 1
+
+# nums = [N0, N1, N2, N3, N4, N5, N6, N7]
+
+num_of_clus = np.genfromtxt('Data/number_of_clusters.txt', unpack=True)
+plt.bar(Kanalnummer[:6], clustersize[:6], label="Kanäle pro Cluster", color = 'magenta', alpha=0.6)
+plt.bar(Kanalnummer[:6], num_of_clus[:6], label="Anzahl Cluster Pro Event", color = 'dodgerblue', alpha=0.6)
 plt.legend()
 plt.grid()
 plt.xlabel("Kanal")
@@ -51,15 +80,17 @@ plt.clf()
 
 
 signal, no_entries = np.genfromtxt('Data/signal_quelle.txt', unpack=True, skip_header=1)
+ADC_most_probable = np.argmax(no_entries)
+print(np.mean(no_entries[no_entries > 0]))
 plt.plot(signal, no_entries, marker = '.',  color = 'magenta', linestyle = 'None', label="ADC Counts der $^{90}Sr$ Quelle")
-plt.xlim(0, 350)
+plt.vlines(signal[ADC_most_probable], 0, np.max(no_entries), color='dodgerblue', linestyle='--')
+plt.xlim(30, 250)
 plt.legend()
 plt.grid()
-plt.xlabel("Kanal")
+plt.xlabel("ADC")
 plt.ylabel("Häufigkeit")
 plt.savefig("plots/signal_Quelle.pdf")
 plt.clf()
-
 
 a = 0.00864769757886111
 b = 0.0001310540826792737
@@ -69,16 +100,18 @@ e = 3.474112787015162e-10
 
 umrechnung = []
 
-for i in signal: 
+for i in signal:
     calc = a + b* i + c* i**2 + d* i**3 + e* i**4
     umrechnung.append(calc)
 umrechnung = np.sort(umrechnung)
 
+energy_most_probable = np.argmax(no_entries)
 plt.plot(umrechnung, no_entries, marker = '.',  color = 'magenta', linestyle = 'None', label="Energiespektrum der $^{90}Sr$ Quelle")
-plt.xlim(0, 10)
+plt.vlines(umrechnung[energy_most_probable], 0, np.max(no_entries), color='dodgerblue', linestyle='--')
+plt.xlim(1.5, 6)
 plt.legend()
 plt.grid()
-plt.xlabel("Energie in XXX")
+plt.xlabel("Energie in keV")
 plt.ylabel("Häufigkeit")
 plt.savefig("plots/signal_Quelle_energie.pdf")
 plt.clf()
